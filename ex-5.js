948ba5bc23ad056b71af8ceeca3a75c8b4bc9542
PNKim <paljapon.nait@gmail.com>
2024-05-09 13:18:04 +0700

    feat: resolve ex1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalSubPrice = 27000;
+let discount = (totalSubPrice * 25) / 100;
+let totalPrice = totalSubPrice - discount;
+
+console.log(totalPrice);
